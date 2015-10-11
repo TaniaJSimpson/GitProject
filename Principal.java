1da6d72d952a6818ae469c1b946a5370fae573ea
TaniaJSimpson <litvinenkotania@gmail.com>
2015-10-11 04:16:13 +0200

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -17,7 +17,7 @@ public class Principal {
 		ArrayList array = new ArrayList();
 		List<Character> lletres = new ArrayList<>();
 		List<Integer> numeros = new ArrayList<>();
-
+			//CONFLICTEWEB
 		while (car < frase.length()) {
 			com = frase.charAt(car);
 
